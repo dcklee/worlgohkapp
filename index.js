690963240cfe6d75e9c71627175676612d473940
dcklee <dcklee11@gmail.com>
2020-04-12 16:26:38 +0800

    Rename 25th Apr 2020

--- index.js
+++ index.js
@@ -27,7 +27,47 @@ app.use(express.static(path.join(__dirname, "public")));
  */
 
 app.get("/", (req, res) => {
-  res.render("index", {title: "Home" });
+  res.render("demo-1", {title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {title: "About Us" });
+});
+
+app.get("/blog-listing", (req, res) => {
+  res.render("blog-listing", {title: "Blog Listing" });
+});
+
+app.get("/contacts-1", (req, res) => {
+  res.render("contacts-1", {title: "Contact Us" });
+});
+
+app.get("/country-details", (req, res) => {
+  res.render("country-details", {title: "Country Details" });
+});
+
+app.get("/courses-list", (req, res) => {
+  res.render("courses-list", {title: "Courses List" });
+});
+
+app.get("/faqs", (req, res) => {
+  res.render("faqs", {title: "FAQ's" });
+});
+
+app.get("/partners", (req, res) => {
+  res.render("partners", {title: "Partners" });
+});
+
+app.get("/single-post", (req, res) => {
+  res.render("single-post", {title: "Single Post" });
+});
+
+app.get("/visa-details", (req, res) => {
+  res.render("visa-details", {title: "Visa Details" });
+});
+
+app.get("/visa-list-1", (req, res) => {
+  res.render("visa-list-1", {title: "Visa List" });
 });
 
 app.get("/user", (req, res) => {
