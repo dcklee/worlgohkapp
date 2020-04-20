44f49263219fff23c6235c45659316bd1b4a09ef
dcklee <dcklee11@gmail.com>
2020-04-20 16:21:30 +0800

    Major Release Apr 2020 V4.9

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 const express = require("express");
 const path = require("path");
-const compression = require('compression');
 
 /**
  * App Variables
@@ -84,7 +83,6 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(compression());
 
 /**
  * Routes Definitions
