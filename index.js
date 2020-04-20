46f3e43af753389eb7db16125266137e1c61e226
dcklee <dcklee11@gmail.com>
2020-04-20 15:43:29 +0800

    Major Release Apr 2020 V4.7

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ const port = process.env.PORT || "8000";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-//app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join("https://storage.cloud.google.com/worlgohk.appspot.com/", "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
 /**
  * Routes Definitions
  */
